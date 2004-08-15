9a61b1c4506fb2a6a0886f6cbacd58c15bfcd09c
Andrew Hunter <andrew@logicalshift.co.uk>
2004-08-15 14:29:56 +0000

    - fixed a problem with the colour of header text, changed the header text size
    --HG--
    branch : Inform
    extra : convert_revision : svn%3Af2cc7feb-2fdc-0310-9003-87a4627787f1/trunk/Inform%401283

--- IFProjectPane.m
+++ IFProjectPane.m
@@ -34,7 +34,7 @@ static NSDictionary* styles[256];
     NSFont* systemFont       = [NSFont systemFontOfSize: 11];
     NSFont* smallFont        = [NSFont boldSystemFontOfSize: 9];
     NSFont* boldSystemFont   = [NSFont boldSystemFontOfSize: 11];
-    NSFont* headerSystemFont = [NSFont boldSystemFontOfSize: 13];
+    NSFont* headerSystemFont = [NSFont boldSystemFontOfSize: 12];
     NSFont* monospaceFont    = [NSFont fontWithName: @"Monaco"
                                                size: 9];
     
@@ -100,6 +100,7 @@ static NSDictionary* styles[256];
     // Natural inform syntax types
     styles[IFSyntaxHeading] = [[NSDictionary dictionaryWithObjectsAndKeys:
         headerSystemFont, NSFontAttributeName,
+		[NSColor blackColor], NSForegroundColorAttributeName,
         nil] retain];
 	styles[IFSyntaxGameText] = [[NSDictionary dictionaryWithObjectsAndKeys:
         boldSystemFont, NSFontAttributeName,
