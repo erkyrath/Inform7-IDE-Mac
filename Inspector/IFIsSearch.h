989370c242e74a98c8c4e7cf88388a85be4de477
Andrew Hunter <andrew@logicalshift.co.uk>
2005-01-29 17:12:36 +0000

    - bound search inspector controls to the inspector object  - search inspector options are now saved/loaded to/from the defaults file
    --HG--
    branch : Inform
    extra : convert_revision : svn%3Af2cc7feb-2fdc-0310-9003-87a4627787f1/trunk/Inform%401494

--- Inspector/IFIsSearch.h
+++ Inspector/IFIsSearch.h
@@ -16,11 +16,38 @@
 extern NSString* IFIsSearchInspector;
 
 @interface IFIsSearch : IFInspector {
+	// Active window and project
 	NSWindow* activeWin;
 	IFProject* activeProject;
 	IFProjectController* activeController;	
+	
+	// Search controls
+	IBOutlet NSTextField* searchText;
+	IBOutlet NSButton* searchButton;
+	
+	IBOutlet NSPopUpButton* searchType;
+
+	IBOutlet NSButton* caseSensitive;
+	IBOutlet NSButton* searchDocs;
+	IBOutlet NSButton* searchSources;
+	IBOutlet NSButton* searchExtensions;
+	
+	// Current options
+	int willSearchType;
+	BOOL willCaseSensitive;
+	BOOL willSearchSources;
+	BOOL willSearchExtensions;
+	BOOL willSearchDocs;
 }
 
+// The shared search inspector
 + (IFIsSearch*) sharedIFIsSearch;
+
+// Dealing with the controls
+- (void) setControlsFromSettings;
+
+// Search actions
+- (IBAction) startSearch: (id) sender;
+- (IBAction) changeSearchOption: (id) sender;
 
 @end
--- Inspector/IFIsSearch.m
+++ Inspector/IFIsSearch.m
@@ -8,11 +8,31 @@
 
 #import "IFIsSearch.h"
 
+// Inspector key
 NSString* IFIsSearchInspector = @"IFIsSearchInspector";
+
+// Defaults keys
+NSString* IFIsSearchCaseSensitive	= @"IFIsSearchCaseSensitive";
+NSString* IFIsSearchDocs			= @"IFIsSearchSearchDocs";
+NSString* IFIsSearchSource			= @"IFIsSearchSource";
+NSString* IFIsSearchExtensions		= @"IFIsSearchExtensions";
+NSString* IFIsSearchType			= @"IFIsSearchType";
 
 @implementation IFIsSearch
 
 // = Initialisation =
+
++ (void) initialize {
+	// Set up the user defaults
+	[[NSUserDefaults standardUserDefaults] registerDefaults: 
+		[NSDictionary dictionaryWithObjectsAndKeys: 
+			[NSNumber numberWithBool: NO], IFIsSearchCaseSensitive,
+			[NSNumber numberWithBool: YES], IFIsSearchDocs, 
+			[NSNumber numberWithBool: YES], IFIsSearchSource, 
+			[NSNumber numberWithBool: NO], IFIsSearchExtensions, 
+			[NSNumber numberWithInt: 1], IFIsSearchType, 
+			nil]];
+}
 
 + (IFIsSearch*) sharedIFIsSearch {
 	IFIsSearch* sharedSearch = nil;
@@ -33,6 +53,17 @@ NSString* IFIsSearchInspector = @"IFIsSearchInspector";
 		[self setTitle: [[NSBundle mainBundle] localizedStringForKey: @"Inspector Search"
 															   value: @"Search"
 															   table: nil]];
+		
+		// Read the settings from the defaults
+		NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+		
+		willSearchType = [[defaults objectForKey: IFIsSearchType] intValue];
+		willCaseSensitive = [[defaults objectForKey: IFIsSearchCaseSensitive] boolValue];
+		willSearchSources = [[defaults objectForKey: IFIsSearchSource] boolValue];
+		willSearchExtensions = [[defaults objectForKey: IFIsSearchExtensions] boolValue];
+		willSearchDocs = [[defaults objectForKey: IFIsSearchDocs] boolValue];
+		
+		[self setControlsFromSettings];
 	}
 	
 	return self;
@@ -65,6 +96,55 @@ NSString* IFIsSearchInspector = @"IFIsSearchInspector";
 
 - (BOOL) available {
 	return activeProject==nil?NO:YES;
+}
+
+// = Running the search inspector =
+
+- (void) setControlsFromSettings {
+	if (willSearchType < 1 || willSearchType > 3) willSearchType = 1;
+	
+	// Set up the controls
+	[searchType selectItem: [[searchType menu] itemWithTag: willSearchType]];
+	[caseSensitive setState: willCaseSensitive?NSOnState:NSOffState];
+	
+	[searchSources setState: willSearchSources?NSOnState:NSOffState];
+	[searchExtensions setState: willSearchExtensions?NSOnState:NSOffState];
+	[searchDocs setState: willSearchDocs?NSOnState:NSOffState];
+}
+
+- (void) saveSettingsToDefaults {
+	NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+
+	[defaults setObject: [NSNumber numberWithInt: willSearchType]
+				 forKey: IFIsSearchType];
+	[defaults setObject: [NSNumber numberWithBool: willCaseSensitive]
+				 forKey: IFIsSearchCaseSensitive];
+
+	[defaults setObject: [NSNumber numberWithBool: willSearchDocs]
+				 forKey: IFIsSearchDocs];
+	[defaults setObject: [NSNumber numberWithBool: willSearchSources]
+				 forKey: IFIsSearchSource];
+	[defaults setObject: [NSNumber numberWithBool: willSearchExtensions]
+				 forKey: IFIsSearchExtensions];
+}
+
+- (IBAction) startSearch: (id) sender {
+}
+
+- (IBAction) changeSearchOption: (id) sender {
+	// Read the controls
+	willSearchType = [[searchType selectedItem] tag];
+	willCaseSensitive = [caseSensitive state]==NSOnState;
+	
+	willSearchSources = [searchSources state]==NSOnState;
+	willSearchExtensions = [searchExtensions state]==NSOnState;
+	willSearchDocs = [searchDocs state]==NSOnState;
+	
+	// Save into the user defaults
+	[self saveSettingsToDefaults];
+	
+	// Update to the 'real' settings
+	[self setControlsFromSettings];
 }
 
 @end
