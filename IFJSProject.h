dea4113d905efe8de4218fb99f6ccb3a01c0b5cc
Andrew Hunter <andrew@logicalshift.co.uk>
2005-08-29 21:20:38 +0000

    - begun work on a JavaScript class to allow the documentation to control    the project to a certain extent.
    --HG--
    branch : Inform
    extra : convert_revision : svn%3Af2cc7feb-2fdc-0310-9003-87a4627787f1/trunk/Inform%401837

---
+++ IFJSProject.h
@@ -0,0 +1,21 @@
+//
+//  IFJSProject.h
+//  Inform-xc2
+//
+//  Created by Andrew Hunter on 29/08/2005.
+//  Copyright 2005 Andrew Hunter. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+//
+// Class designed to provide a JavaScript interface to a project window.
+//
+// This should make it possible to create buttons that, for example, paste code into the source window.
+//
+@interface IFJSProject : NSObject {
+
+}
+
+@end
---
+++ IFJSProject.m
@@ -0,0 +1,14 @@
+//
+//  IFJSProject.m
+//  Inform-xc2
+//
+//  Created by Andrew Hunter on 29/08/2005.
+//  Copyright 2005 Andrew Hunter. All rights reserved.
+//
+
+#import "IFJSProject.h"
+
+
+@implementation IFJSProject
+
+@end
