118da7fc84a367f8a7cce4fc39e2363f0dce75f1
Andrew Hunter <andrew@logicalshift.co.uk>
2005-10-22 11:34:05 +0000

    - added missing files
    --HG--
    branch : Inform
    extra : convert_revision : svn%3Af2cc7feb-2fdc-0310-9003-87a4627787f1/trunk/Inform%401919

---
+++ Spotlight/MetadataImporter.h
@@ -0,0 +1,14 @@
+//
+//  MetadataImporter.h
+//  Inform-xc2
+//
+//  Created by Andrew Hunter on 17/10/2005.
+//  Copyright 2005 Andrew Hunter. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <CoreFoundation/CoreFoundation.h>
+#import <CoreFoundation/CFPlugInCOM.h>
+#import <CoreServices/CoreServices.h>
+#import <Foundation/Foundation.h>
