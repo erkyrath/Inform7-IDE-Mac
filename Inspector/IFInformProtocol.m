788ce6d3c67217af2fe4de7091bae51d9abe0f4c
Andrew Hunter <andrew@logicalshift.co.uk>
2006-04-24 23:21:58 +0000

    - implemented the extension documentation directory
    --HG--
    branch : Inform
    extra : convert_revision : svn%3Af2cc7feb-2fdc-0310-9003-87a4627787f1/trunk/Inform%402016

--- Inspector/IFInformProtocol.m
+++ Inspector/IFInformProtocol.m
@@ -60,13 +60,68 @@
 - (void) startLoading {
 	// Might as well load the whole file at once
 	NSString* urlPath = [[theURLRequest URL] path];
+	NSString* host = [[theURLRequest URL] host];
 	NSString* path;
 	
-	// Try using pathForResource:ofType:
-	// Advantage of this is that it will allow for localisation at some point in the future
 	// Note: first character will always be '/', hence the 'substring' thing
-	path = [[NSBundle mainBundle] pathForResource: [[urlPath substringFromIndex: 1] stringByDeletingPathExtension]
-										   ofType: [urlPath pathExtension]];
+	urlPath = [urlPath substringFromIndex: 1];
+	
+	NSArray* components = [urlPath pathComponents];
+	
+	// Accept either the host or the path specifier containing 'extensions'
+	if ([[host lowercaseString] isEqualToString: @"extensions"] ||
+			(components != nil && [components count] > 1 && 
+			 [[[components objectAtIndex: 0] lowercaseString] isEqualToString: @"extensions"])) {
+		int skip = 0;
+		int x;
+		
+		if (![[host lowercaseString] isEqualToString: @"extensions"])
+			skip = 1;
+		
+		// Try the library directories
+		NSArray* libraries = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+		NSEnumerator* libEnum = [libraries objectEnumerator];
+		NSString* library;
+		
+		while (library = [libEnum nextObject]) {
+			NSEnumerator* componentEnum = [components objectEnumerator];
+			NSString* pathComponent;
+			
+			path = [[library stringByAppendingPathComponent: @"Inform"] stringByAppendingPathComponent: @"Documentation"];
+			for (x=0; x<skip; x++) [componentEnum nextObject];
+			while (pathComponent = [componentEnum nextObject]) {
+				path = [path stringByAppendingPathComponent: pathComponent];
+			}
+			
+			if ([[NSFileManager defaultManager] fileExistsAtPath: path]) {
+				break;
+			} else {
+				path = nil;
+			}
+		}
+		
+		// Try the ExtnDocs resources directory
+		if (path == nil) {
+			NSEnumerator* componentEnum = [components objectEnumerator];
+			NSString* pathComponent;
+			
+			path = @"ExtnDocs";
+			for (x=0; x<skip; x++) [componentEnum nextObject];
+			while (pathComponent = [componentEnum nextObject]) {
+				path = [path stringByAppendingPathComponent: pathComponent];
+			}
+			
+			path = [[NSBundle mainBundle] pathForResource: [[path lastPathComponent] stringByDeletingPathExtension]
+												   ofType: [path pathExtension]
+											  inDirectory: [path stringByDeletingLastPathComponent]];
+		}
+	} else {
+		// Try using pathForResource:ofType:
+		// Advantage of this is that it will allow for localisation at some point in the future
+		path = [[NSBundle mainBundle] pathForResource: [urlPath stringByDeletingPathExtension]
+											   ofType: [urlPath pathExtension]];
+	}
+
 	if (path == nil) {
 		// If that fails, then just append to the resourcePath of the main bundle
 		path = [[[[NSBundle mainBundle] resourcePath] stringByAppendingString: @"/"] stringByAppendingString: urlPath];
