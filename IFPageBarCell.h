a34771da4917b67b9e78cf9b3373f3f0abc17a6a
Andrew Hunter <andrew@logicalshift.co.uk>
2007-04-09 17:35:14 +0000

    - added page bar cell class
    --HG--
    branch : Inform
    extra : convert_revision : svn%3Af2cc7feb-2fdc-0310-9003-87a4627787f1/trunk/Inform%402466

---
+++ IFPageBarCell.h
@@ -0,0 +1,19 @@
+//
+//  IFPageBarCell.h
+//  Inform-xc2
+//
+//  Created by Andrew Hunter on 06/04/2007.
+//  Copyright 2007 Andrew Hunter. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+//
+// A cell that can be placed on the left or right of the IFPageBarView
+//
+@interface IFPageBarCell : NSActionCell {
+
+}
+
+@end
---
+++ IFPageBarCell.m
@@ -0,0 +1,14 @@
+//
+//  IFPageBarCell.m
+//  Inform-xc2
+//
+//  Created by Andrew Hunter on 06/04/2007.
+//  Copyright 2007 Andrew Hunter. All rights reserved.
+//
+
+#import "IFPageBarCell.h"
+
+
+@implementation IFPageBarCell
+
+@end
